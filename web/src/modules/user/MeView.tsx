faa4350ef27398b045bc2d31c6baa4b854110435
bluedeadlx <arnoldmanuel77@gmail.com>
2019-02-23 17:38:08 +0100

    added query for getting user

--- web/src/modules/user/MeView.tsx
+++ web/src/modules/user/MeView.tsx
@@ -1,5 +1,35 @@
+import gql from "graphql-tag";
 import * as React from "react";
+import { Query } from "react-apollo";
+import { MeQuery } from "../../schemaTypes";
+
+const meQuery = gql`
+  query MeQuery {
+    me {
+      email
+      fullName
+    }
+  }
+`;
 
 export const MeView = () => {
-  return <div>Me view</div>;
+  return (
+    <Query<MeQuery> query={meQuery}>
+      {({ data, loading, error }) => {
+        if (error) {
+          return <div>There was an error</div>;
+        }
+        if (loading) {
+          return <div>loading...</div>;
+        }
+        if (!data) {
+          return <div>Something went wrong</div>;
+        }
+        if (!data.me) {
+          return <div>Something went wrong</div>;
+        }
+        return <div>Welcome back, {data.me.fullName}</div>;
+      }}
+    </Query>
+  );
 };
